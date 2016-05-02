76e41cfd4af758b20df88de1e65d3a79cbf4410f
dariusz.bacinski <darek@macoscope.net>
2016-05-02 13:54:17 +0200

    [MCLAB] Testing RxJava - to blocking

---
+++ app/src/test/groovy/com/macoscope/unittesting/rxjava/RxToBlockingSpec.groovy
@@ -0,0 +1,53 @@
+package com.macoscope.unittesting.rxjava
+
+import rx.Observable
+import rx.functions.Func0
+import rx.schedulers.Schedulers
+import spock.lang.Specification
+import spock.lang.Timeout
+
+public class RxToBlockingSpec extends Specification {
+
+    def 'io scheduler no delay'() {
+        when:
+            List<String> result = createObservableWithTextAndDelay("John", 0)
+                    .subscribeOn(Schedulers.io())
+                    .toList().toBlocking().single();
+        then:
+            //sleep 10
+            result.contains("John")
+    }
+
+
+    def 'io scheduler with 1s delay'() {
+        when:
+            List<String> result = createObservableWithTextAndDelay("John", 1000)
+                    .subscribeOn(Schedulers.io())
+                    .toList().toBlocking().single();
+        then:
+            //sleep 1010
+            result.contains("John")
+    }
+
+    @Timeout(10)
+    def 'infinite io scheduler no delay'() {
+        when:
+            List<String> result = createObservableWithTextAndDelay("John", 0)
+                    .repeat()
+                    .subscribeOn(Schedulers.io())
+                    .toList().toBlocking().single();
+        then:
+            //toBlocking().single() is waiting for sequence complete
+            result.contains("John")
+    }
+
+    private Observable<String> createObservableWithTextAndDelay(String text, long delay) {
+        Observable.defer(new Func0<Observable>() {
+            @Override
+            Observable call() {
+                sleep(delay)
+                return Observable.just(text, text)
+            }
+        })
+    }
+}
--- app/src/test/groovy/com/macoscope/unittesting/rxjava/RxToBlockingSpec.java
+++
@@ -1,6 +0,0 @@
-package com.macoscope.unittesting.rxjava;
-
-public class RxToBlockingSpec {
-
-
-}
